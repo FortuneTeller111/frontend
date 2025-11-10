9087987b6b8c937db79c843dce30199ddc4de0e8
x402sassy <x402sassy@tutamail.com>
2025-11-10 20:49:56 +0100

    added navbar with solana wallet connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Tarot Reading
-      </h1>
+      <Navbar/>
     </div>
   );
 }
