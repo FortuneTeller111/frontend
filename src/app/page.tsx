316b5a08d05e5b381824ac0bc4d3002a82f3cce9
x402sassy <x402sassy@tutamail.com>
2025-11-11 14:58:20 +0100

    fix: component changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer";
-import IntegratedWalletPayment from "@/components/IntegratedWalletPayment";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -8,9 +7,6 @@ export default function Home() {
       <main
         className="min-h-screen w-full flex items-center justify-center px-4">
         <Navbar />
-          <div className="pt-30">
-          <IntegratedWalletPayment />
-        </div>
         <Footer />
       </main>
     </>
