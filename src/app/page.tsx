5c3e205fd6cff2100324a8c0ffc5b7fce99a9430
x402sassy <x402sassy@tutamail.com>
2025-11-11 15:12:11 +0100

    added env var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/components/Footer";
-import IntegratedWalletPayment from "@/components/IntegratedWalletPayment";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
       <main
         className="min-h-screen w-full flex items-center justify-center px-4">
         <Navbar />
-        <IntegratedWalletPayment/>
         <Footer />
       </main>
     </>
