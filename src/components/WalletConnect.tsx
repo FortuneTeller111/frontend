295137729dff4c4c7b98b6c44774755941f85c0a
x402sassy <x402sassy@tutamail.com>
2025-11-11 14:54:50 +0100

    fix: wallet solana connection

---
+++ src/components/WalletConnect.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from 'next/dynamic';
+
+
+// Prevent SSR for wallet button
+const WalletMultiButtonDynamic = dynamic(
+  async () => (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+);
+
+const WalletConnect = () => {
+  return (
+    <div className="relative w-[187px] h-[57px]">
+      <img 
+        src="/images/wallet.png" 
+        className="absolute inset-0 w-full h-full pointer-events-none"
+        alt="wallet background"
+      />
+      <WalletMultiButtonDynamic 
+        className="bg-[url(/images/wallet.png)]! border-none! w-[187px]! h-[57px]!"
+        style={{
+          width: '187px',
+          height: '57px',
+          background: 'transparent',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      />
+    </div>
+  );
+};
+
+export default WalletConnect;
